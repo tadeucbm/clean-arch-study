6d5f7a0b288ef25676139d1846b019a225f30c12
Tadeu Madureira <tadeucbmad@gmail.com>
2025-06-04 22:20:11 -0400

    adding user_register

---
+++ src/data/use_cases/user_register_test.py
@@ -0,0 +1,34 @@
+from src.infra.db.tests.users_repository import UsersRepositorySpy
+from .user_register import UserRegister
+
+def test_register():
+    first_name = "Ola"
+    last_name = "aqui"
+    age = 3
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    response = user_register.register(first_name, last_name, age)
+
+    assert repo.insert_user_atributes["first_name"] == first_name
+    assert repo.insert_user_atributes["last_name"] == last_name
+    assert repo.insert_user_atributes["age"] == age
+
+    assert response["type"] == "Users"
+    assert response["count"] == 1
+    assert response["attributes"]
+
+def test_register_first_name_error():
+    first_name = "12345"
+    last_name = "aqui"
+    age = 3
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    try:
+        user_register.register(first_name, last_name, age)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Nome inválido para busca"
---
+++ src/domain/use_cases/user_register.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+
+class UserRegister(ABC):
+
+    @abstractmethod
+    def register(first_name: str, last_name:str, age: int) -> Dict:
+        pass
