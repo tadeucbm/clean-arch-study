c78f60b5819939a482cc9304d44c07d73bf54c3e
Tadeu Madureira <tadeucbmad@gmail.com>
2025-06-23 01:09:58 -0400

    fix format from users_repository

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -1,25 +1,23 @@
-from src.infra.db.settings.connection import DBConnectionHandler
-from src.infra.db.entities.users import Users as UsersEntity
-from src.data.interfaces.users_repository import UsersRepositoryInterface
-from src.domain.models.users import Users
-
 from typing import List
 
-class UsersRepository(UsersRepositoryInterface):
+from src.data.interfaces.users_repository import UsersRepositoryInterface
+from src.domain.models.users import Users
+from src.infra.db.entities.users import Users as UsersEntity
+from src.infra.db.settings.connection import DBConnectionHandler
 
+
+class UsersRepository(UsersRepositoryInterface):
     @classmethod
     def insert_user(cls, first_name: str, last_name: str, age: int) -> None:
         with DBConnectionHandler() as database:
             try:
                 new_registry = UsersEntity(
-                    first_name=first_name,
-                    last_name=last_name,
-                    age=age
+                    first_name=first_name, last_name=last_name, age=age
                 )
                 database.session.add(new_registry)
                 database.session.commit()
 
-            except Exception as exception:
+            except Exception:
                 database.session.rollback()
                 raise Exception
 
@@ -28,13 +26,12 @@ class UsersRepository(UsersRepositoryInterface):
         with DBConnectionHandler() as database:
             try:
                 users = (
-                    database.session
-                        .query(UsersEntity)
-                        .filter(UsersEntity.first_name == first_name)
-                        .all()
+                    database.session.query(UsersEntity)
+                    .filter(UsersEntity.first_name == first_name)
+                    .all()
                 )
                 return users
 
-            except Exception as exception:
+            except Exception:
                 database.session.rollback()
                 raise Exception
