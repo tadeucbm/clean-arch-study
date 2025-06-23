83dcf4bedd3a886aeb055378d807e2649292c9ad
Tadeu Madureira <tadeucbmad@gmail.com>
2025-06-23 01:11:08 -0400

    fix format from interfaces

--- src/data/interfaces/users_repository.py
+++ src/data/interfaces/users_repository.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
-from src.domain.models.users import Users
-
 from typing import List
+
+from src.domain.models.users import Users
 
 
 class UsersRepositoryInterface(ABC):
+    @abstractmethod
+    def insert_user(cls, first_name: str, last_name: str, age: int) -> None:
+        pass
 
     @abstractmethod
-    def insert_user(cls, first_name: str, last_name: str, age: int) -> None: pass
-
-    @abstractmethod
-    def get_user(cls, first_name: str) -> List[Users]: pass
+    def get_user(cls, first_name: str) -> List[Users]:
+        pass
